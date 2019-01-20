c081f80ca59624856fa687b51fae8b5a0d37ad7f
MrEliptik <victormeunier.dev@gmail.com>
2019-01-20 20:02:05 +0200

    board drawn

--- snake.js
+++ snake.js
@@ -1,9 +1,28 @@
-canvas = document.getElementById("canvas")
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
 
-gridRows = 12
-gridColumns = 12
-grid = [[]]
+canvasWidth = canvasHeight = 600;
 
-for(let i = 0 ; i < gridRows ; i++){
-    
+gridRows = 12;
+gridColumns = 12;
+grid = [[]];
+p=10;
+
+function drawBoard(gridRows, gridColumns, canvasHeight, canvasWidth){
+    ctx.beginPath();
+
+    for(var i = 0; i <= gridRows; i++){
+        ctx.moveTo(0, (canvasHeight/gridRows)*i);
+        ctx.lineTo(canvasWidth, (canvasHeight/gridRows)*i);
+    }
+
+    for(var i = 0; i <= gridColumns; i++){
+        ctx.moveTo((canvasWidth/gridColumns)*i, 0);
+        ctx.lineTo((canvasWidth/gridColumns)*i, canvasHeight);
+    }
+
+    ctx.strokeStyle = "black";
+    ctx.stroke();
 }
+
+drawBoard(gridRows, gridColumns, canvasHeight, canvasWidth);
