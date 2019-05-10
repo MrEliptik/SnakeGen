f124ea76a61abae7ae5d111fa73e75949e688984
MrEliptik <victormeunier.dev@gmail.com>
2019-05-10 22:06:37 +0200

    add sliders and functions to control nb of created games

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-var slider_games = document.getElementById("slider_games");
-var input_games = document.getElementById("input_slider_games");
+var slider_population = document.getElementById("slider_population");
+var slider_games_visible = document.getElementById("slider_games_visible");
+var input_population = document.getElementById("input_slider_population");
+var input_games_visible = document.getElementById("input_slider_games_visble");
 var canvas_container = document.querySelector(".canvas-container");
 var btn_create = document.getElementById("btn_create");
+var btn_delete = document.getElementById("btn_delete");
+var btn_default = document.getElementById("btn_default");
+var radios_speed = document.getElementsByName("speed");
 
 var games = [];
+var speed = 1;
 
 //var game = new Game( 12, 12, 100, 600, 600, canvas.getContext("2d"), 1, 1, "DF", true); // default mode, display on
 //var game = new Game( 12, 12, 100, 600, 600, canvas.getContext("2d"), 1, 1, "DF", false); // default mode, display off
@@ -25,17 +31,65 @@ var nn = new NeuralNetwork(11, 100, 3);
    }, 1000);
    */
 
-function createGames() {
-  for (var i = 0; i < input_games.value; i++) {
-    var canvas = document.createElement("canvas");
-    canvas.id = "canvas_" + String(i);
-    canvas.width = 100;
-    canvas.height = 100;
-    canvas_container.appendChild(canvas);
+function allDefaultUI() {}
 
-    games.push(
-      new Game(12, 12, 100, 100, 100, canvas, 1, 1, "DF", true)
-    );
+function createGames() {
+  // First delete previously created games
+  deleteGames();
+  var visible = 0;
+  for (var i = 0; i < input_population.value; i++) {
+    if(visible < input_games_visible.value){
+      var canvas = document.createElement("canvas");
+      canvas.id = "canvas_" + String(i);
+      if(canvas_container.offsetWidth / input_population.value < 90) canvas.width = 90;
+      else canvas.width = canvas_container.offsetWidth / input_population.value;
+      canvas.height = canvas.width;
+      canvas_container.appendChild(canvas);
+      games.push(
+        new Game(12, 12, canvas.height, canvas.width, canvas, 1, 1, "DF", true)
+      );
+      visible++;
+    }
+    else{
+      games.push(
+        new Game(12, 12, canvas.height, canvas.width, canvas, 1, 1, "DF", false)
+      );
+    }
+    
+  }
+}
+
+function deleteGames() {
+  // Ask for user's confirmation first
+  if (games.length > 0) {
+    if (
+      askUserConfirmation(
+        "This is going to delete currently created games, are you sure?"
+      )
+    ) {
+      // Remove canvas from container
+      while (canvas_container.firstChild) {
+        canvas_container.removeChild(canvas_container.firstChild);
+      }
+      // Empty the games array object
+      games = [];
+    }
+  }
+}
+
+function askUserConfirmation(msg) {
+  return confirm(msg);
+}
+
+function getSpeedValue() {
+  for (var i = 0, length = radios.length; i < length; i++) {
+    if (radios[i].checked) {
+      // do whatever you want with the checked radio
+      alert(radios[i].value);
+
+      // only one radio can be logically checked, don't check the rest
+      break;
+    }
   }
 }
 
@@ -46,13 +100,21 @@ document.addEventListener(
     const key = event.keyCode;
 
     if (key == "38") {
-      game.update(0, "up");
+      games.forEach(game => {
+        game.update(0, "up");
+      });
     } else if (key == "40") {
-      game.update(0, "down");
+      games.forEach(game => {
+        game.update(0, "down");
+      });
     } else if (key == "37") {
-      game.update(0, "left");
+      games.forEach(game => {
+        game.update(0, "left");
+      });
     } else if (key == "39") {
-      game.update(0, "right");
+      games.forEach(game => {
+        game.update(0, "right");
+      });
     }
   },
   false
@@ -66,8 +128,8 @@ window.addEventListener(
     //canvas.width = canvas_container.offsetWidth;
     //canvas.height = canvas_container.offsetHeight;
 
-    canvas.width = (document.documentElement.clientWidth * 2) / 3;
-    canvas.height = (document.documentElement.clientHeight * 2) / 3;
+    //canvas.width = (document.documentElement.clientWidth * 2) / 3;
+    //canvas.height = (document.documentElement.clientHeight * 2) / 3;
 
     games.forEach(game => {
       if (game.display) {
@@ -78,10 +140,28 @@ window.addEventListener(
   false
 );
 
-slider_games.addEventListener("change", () => {
-  input_games.value = slider_games.value;
+slider_population.addEventListener("change", () => {
+  input_population.value = slider_population.value;
+});
+
+slider_games_visible.addEventListener("change", () => {
+  input_games_visible.value = slider_games_visible.value;
 });
 
 btn_create.addEventListener("click", () => {
   createGames();
 });
+
+btn_delete.addEventListener("click", () => {
+  deleteGames();
+});
+
+btn_default.addEventListener("click", () => {
+  allDefaultUI();
+});
+
+for (var i = 0, max = radios_speed.length; i < max; i++) {
+  radios_speed[i].onclick = function() {
+    speed = this.value;
+  };
+}
