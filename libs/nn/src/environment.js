c4dd3e7e9f8e6713d99d582878c5ceacc1531bc8
MrEliptik <victormeunier.dev@gmail.com>
2019-05-22 14:39:52 +0200

    reset all agents' game each gen

--- libs/nn/src/environment.js
+++ libs/nn/src/environment.js
@@ -28,7 +28,7 @@ class Environment extends Generation {
       selectionPerCentage,
       stepSizeParameter,
       mutationProb)
-    
+
     this.speed = speed;
     this.tickout = tickout;
     this.state = state;
@@ -83,83 +83,89 @@ class Environment extends Generation {
     this.tickCount = 0;
   }
 
-  getAllScores(){
+  getAllScores() {
     var scores = [];
     var that = this;
-    for(let i = 0; i < this.agents.length ; i++){
+    for (let i = 0; i < this.agents.length; i++) {
       scores.push(this.agents[i].getScore());
     }
     return scores;
   }
 
-  getHighestScore(){
+  getHighestScore() {
     //notImplemented
   }
 
-  getCurrGenHighestScore()
-  {
-    if(this.currGenHighScore < Math.max(...this.getAllScores())){
+  getCurrGenHighestScore() {
+    if (this.currGenHighScore < Math.max(...this.getAllScores())) {
       this.currGenHighScore = Math.max(...this.getAllScores());
     }
     return this.currGenHighScore;
   }
 
-  getCurrGenID(){
+  getCurrGenID() {
     return this.id;
   }
-    
-  setPlayPauseState(state){
+
+  setPlayPauseState(state) {
     this.state = state;
   }
 
-  getPlayPauseState(){
+  getPlayPauseState() {
     return this.state;
   }
 
-  setSpeed(speed){
+  setSpeed(speed) {
     this.speed = speed;
   }
 
-  getTickCount(){
+  getTickCount() {
     return this.tickCount;
   }
 
-  tick(){
+  tick() {
     // Tell each agent to take one step in the
     // game
     this.agents.forEach(agent => {
       // if return false, agent is dead
-      if(!agent.step()){
+      if (!agent.step()) {
         this.agentsAlive--;
       }
     });
 
-     return this.agentAlive != 0;
+    return this.agentAlive != 0;
   }
 
-  update(){
-
-    if(this.state == "play") {
+  update() {
+    if (this.state == "play") {
       return;
     }
 
-    if(this.tickCount < this.tickout) {
-     
-        // No agents left, next gen
-        if(!this.tick()) {
-          this.agents = this.createNextGen(this.agents);
-          this.tickCount = 0;
-        }
+    if (this.tickCount < this.tickout) {
+
+      // No agents left, next gen
+      if (!this.tick()) {
+        this.agents = this.createNextGen(this.agents);
+        this.tickCount = 0;
+        // reset games
+        this.agents.forEach(agent => {
+          agent.resetGame();
+        });
+      }
     } else {
       this.agents = this.createNextGen(this.agents);
       this.tickCount = 0;
+      // reset games
+      this.agents.forEach(agent => {
+        agent.resetGame();
+      });
     }
 
     var that = this;
-      
-    setTimeout(function(){
+
+    setTimeout(function () {
       that.tickCount += 1;
       that.update();
-    }, 1000/this.speed);
+    }, 1000 / this.speed);
   }
 }
