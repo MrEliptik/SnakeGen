22f6b4741d5e6d48305a3ce9d6b8c84f3d10cb52
Victor Meunier <mreliptik@pop-os.localdomain>
2019-05-24 21:22:04 +0200

    modify calculated value for conesOfSight

--- tests/tests_game.js
+++ tests/tests_game.js
@@ -8,7 +8,8 @@ console.assert(JSON.stringify(test_calculateLineOfSight(10, [5,5], "left")) === 
 
 console.log(">>> Test calculateLinesOfSight");
 // TODO
-console.assert(JSON.stringify(calculateConesOfSight(10, [5,5], "up", [0, 5])) === JSON.stringify([0.4, 0.5, 0.5]), {error:"[ERROR] Calculated COS don't correspond"});
+console.assert(JSON.stringify(test_calculateConesOfSight(10, [5,5], "up", [5, 0])) === JSON.stringify([[0.273972602739726, 0], [0.273972602739726, 1], [0.2602739726027397, 1], [0.1917808219178082, 0]]), {error:"[ERROR] Calculated COS don't correspond"});
+console.assert(JSON.stringify(test_calculateConesOfSight(10, [5,5], "left", [5, 0])) === JSON.stringify([[14/59, 0], [19/59, 0], [20/59, 0], [20/59, 1]]), {error:"[ERROR] Calculated COS don't correspond"});
 
 console.log("[INFO] Testing done!");
 
